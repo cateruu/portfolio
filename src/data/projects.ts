65e372a8b2f4e8192fd716a165a74ab8e43a3348
cateruu <cater981pl@gmail.com>
2023-06-16 22:18:00 +0200

    skills section

---
+++ src/data/projects.ts
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    title: 'Motoradar',
+    subTitle: 'Car marketplace',
+    icon: '/icons/car.svg',
+    link: 'https://github.com/chmery/motoradar',
+  },
+  {
+    title: 'F1 Info',
+    subTitle: 'Informations from F1 world',
+    icon: '/icons/steering-wheel.svg',
+    link: 'https://github.com/cateruu/F1-info-v2',
+  },
+  {
+    title: 'Social',
+    subTitle: 'Social media app inspired by Twitter',
+    icon: '/icons/users.svg',
+    link: 'https://github.com/cateruu/social-app',
+  },
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,58 @@
+export const skills = [
+  {
+    icon: '/skills/react.svg',
+    name: 'React',
+  },
+  {
+    icon: '/skills/nextjs.svg',
+    name: 'NextJS',
+  },
+  {
+    icon: '/skills/astro.svg',
+    name: 'Astro',
+  },
+  {
+    icon: '/skills/typescript.svg',
+    name: 'TypeScript',
+  },
+  {
+    icon: '/skills/redux.svg',
+    name: 'Redux',
+  },
+  {
+    icon: '/skills/tailwindcss.svg',
+    name: 'Tailwind',
+  },
+  {
+    icon: '/skills/sass.svg',
+    name: 'Sass',
+  },
+  {
+    icon: '/skills/css.svg',
+    name: 'Css',
+  },
+  {
+    icon: '/skills/html.svg',
+    name: 'Html',
+  },
+  {
+    icon: '/skills/jest.svg',
+    name: 'Jest',
+  },
+  {
+    icon: '/skills/cypress.svg',
+    name: 'Cypress',
+  },
+  {
+    icon: '/skills/firebase.svg',
+    name: 'Firebase',
+  },
+  {
+    icon: '/skills/git.svg',
+    name: 'Git',
+  },
+  {
+    icon: '/skills/figma.svg',
+    name: 'Figma',
+  },
+];
