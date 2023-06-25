fe0600974241663907280a0d516e8241f689ca28
cateruu <cater981pl@gmail.com>
2023-06-25 13:51:33 +0200

    landing & projects redesign

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,17 +4,20 @@ export const projects = [
     subTitle: 'Car marketplace',
     icon: '/icons/car.svg',
     link: 'https://github.com/chmery/motoradar',
+    color: '#9343AF',
   },
   {
     title: 'F1 Info',
     subTitle: 'Informations from F1 world',
     icon: '/icons/steering-wheel.svg',
     link: 'https://github.com/cateruu/F1-info-v2',
+    color: '#D13C58',
   },
   {
     title: 'Social',
     subTitle: 'Social media app inspired by Twitter',
     icon: '/icons/users.svg',
     link: 'https://github.com/cateruu/social-app',
+    color: '#EB985B',
   },
 ];
